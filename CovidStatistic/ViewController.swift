2ba17f252d119788df7a970826f255a81dc7f95d
batuhanbaran <fb_batuhan_baran@hotmail.com>
2021-07-09 20:41:15 +0300

    Initial Commit

---
+++ CovidStatistic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CovidStatistic
+//
+//  Created by Batuhan Baran on 9.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
